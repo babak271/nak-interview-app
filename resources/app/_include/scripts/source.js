9f8d31c272da653e58e102f44f5cc27b20ef50d6
Babak Ashrafi <52350580+babak271@users.noreply.github.com>
2021-06-03 15:30:52 +0430

    Add twitter bootstrap setup for front

---
+++ resources/app/_include/scripts/source.js
@@ -0,0 +1,11 @@
+module.exports = {
+    js: [
+        'node_modules/jquery/dist/jquery.js',
+        'node_modules/jquery-ui-dist/jquery-ui.js',
+        'node_modules/popper.js/dist/umd/popper.js',
+        'node_modules/bootstrap/dist/js/bootstrap.js',
+        'resource/app/_include/scripts/components/bootstrap-util.js',
+        'resource/app/_include/scripts/initialize.js',
+        'resource/app/_include/scripts/custom.js'
+    ]
+};
